3510c5c9600c5ed1d28e780325352e84fc0583d1
Baranidharan S <baranidharansundharam@gmail.com>
2026-09-16 23:30:27 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyAY1I6tDTqEVW_q4rki164tMIBcF37OT4s",
   authDomain: "agronexus-bdf8b.firebaseapp.com",
   databaseURL: "https://agronexus-bdf8b-default-rtdb.asia-southeast1.firebasedatabase.app",
